23b3d2f2364059d34a6c63a42ac81ae6de9345b7
cam <1743982333@qq.com>
2020-04-03 16:56:34 +0800

    xxx

--- tool/sendSMS.js
+++ tool/sendSMS.js
@@ -1,14 +1,9 @@
-const Core = require('@alicloud/pop-core'); //阿里云短信服务核心包
-
-const validSMSTemplateCode = "SMS_179602114"; //验证码短信模板代码
-const validSMSSignName = "客多达教育"; //验证码短信签名
-const RegionId = "cn-hangzhou"; //验证码短信服务器区域ID
-const client = new Core({
-    accessKeyId: 'LTAI4FwYdSrqPqV4BB9pCQLe',
-    accessKeySecret: 'DJ1GTsqEs6Vz6wygo2txnANlfNZ3cV',
-    endpoint: 'https://dysmsapi.aliyuncs.com',
-    apiVersion: '2017-05-25'
-});
+const {
+    validSMSTemplateCode,
+    validSMSSignName,
+    RegionId,
+    client
+} = require('../config/alicore');
 
 const {
     generateRandomNumber
